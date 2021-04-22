745ea7ca2f035a11a5bb7c2a454573382d67e99d
arnau sarabia reyes <arnau_sarabia@hotmail.com>
2021-04-23 00:37:59 +0200

    codigo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './styles/Navbar.css';
-import logo from '../images/logo.svg';
+import logo from '../images/logo.png';
 
 class Navbar extends React.Component {
   render() {
     return (
       <div className="Navbar">
         <div className="container-fluid">
-          <Link className="Navbar__brand" to="/">
-            <img className="Navbar__brand-logo" src={logo} alt="Logo" />
-            <span className="font-weight-light">Platzi</span>
-            <span className="font-weight-bold">Conf</span>
-          </Link>
+          <div className="row">
+            <Link className="Navbar__brand col-2" to="/">
+              <img className="Navbar__brand-logo" src={logo} alt="Logo" />
+              <span className="color font-weight-light">Tu</span>
+              <span className="color font-weight-bold">Canguraje</span>
+            </Link>
+            <p className="Navbar__title color font-weight-bold col-8"> TU CANGURAJE</p> 
+            <Link className="Navbar__brand col-2" to="/">
+              <img className="Navbar__brand-logo" src={logo} alt="Logo" />
+              <span className="color font-weight-light">Tu</span>
+              <span className="color font-weight-bold">Canguraje</span>
+            </Link>
+          </div>
         </div>
       </div>
     );
--- src/pages/BadgeEdit.js
+++ src/pages/BadgeEdit.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import './styles/BadgeNew.css';
+import './styles/BadgeEdit.css';
 import header from '../images/badge-header.svg';
 import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
-import api from '../api';
+import api from '../api/api';
 import PageLoading from '../components/PageLoading';
 
-class BadgeNew extends React.Component {
+class BadgeEdit extends React.Component {
   state = {
-    loading: false,
+    loading: true,
     error: null,
     form: {
       firstName: '',
@@ -18,6 +18,22 @@ class BadgeNew extends React.Component {
       jobTitle: '',
       twitter: '',
     },
+  };
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData = async e => {
+    this.setState({loading: true, error: null})
+
+    try {
+      const data = await api.badges.read(this.props.match.params.badgeId)
+
+      this.setState({loading: false, form: data });
+    } catch(error) {
+      this.setState({loading: false, error: error});
+    }
   };
 
   handleChange = e => {
@@ -35,7 +51,7 @@ class BadgeNew extends React.Component {
     
 
     try{
-      await api.badges.create(this.state.form);
+      await api.badges.update(this.props.match.params.badgeId, this.state.form);
       this.setState({loading: false});
       this.props.history.push('/badges');
     } catch(error){
@@ -49,13 +65,14 @@ class BadgeNew extends React.Component {
     }
     return (
       <React.Fragment>
-        <div className="BadgeNew__hero">
+        <div className="BadgeEdit__hero">
           <img className="img-fluid" src={header} alt="Logo" />
         </div>
 
         <div className="container">
           <div className="row">
             <div className="col-6">
+              
               <Badge
                 firstName={this.state.form.firstName || 'FIRST NAME'}
                 lastName={this.state.form.lastName || 'LAST NAME'}
@@ -67,7 +84,7 @@ class BadgeNew extends React.Component {
             </div>
 
             <div className="col-6">
-              <h1>New Attendant</h1>
+              <h1>Edit Attendant</h1>
               <BadgeForm
                 onChange={this.handleChange}
                 onSubmit={this.handleSubmit}
@@ -82,4 +99,4 @@ class BadgeNew extends React.Component {
   }
 }
 
-export default BadgeNew;
+export default BadgeEdit;
